7a19e97172d89b104aca5f55083d2fbb2d843d40
Roman Luštrik <romunov@gmail.com>
2017-04-16 20:53:23 +0200

    this function now actually does what it advertises, whooops

--- individualContribution.R
+++ individualContribution.R
@@ -7,13 +7,15 @@
 #' 				the sampling area.
 #' @param effect.distance Numeric. A vector of length 1 noting the maximum
 #' 				distance used to calculate contribution area.
-#' @param ring.weights A vector of values of the curve used to construct the
-#' 			contribution area.
+#' @param bins A data.frame from function `calculateBins`. Data from this source will be used to construct
+#' a parametric curve based on half normal distribution with some additional parameters which aid in
+#' achieving a better fit with regards to longer plateau on the left side of the function. See inline
+#' code comments for more info.
 #' @return A list of contribution values corresponding to individual walker.
 #' @author Roman Luštrik
 
-individualContribution <- function(walk, ...object, .sap.poly, effect.distance, 
-                                   ring.weights, sim.dist, SD, area) {
+individualContribution <- function(walk, ...object, .sap.poly, effect.distance, bins,
+                                   sim.dist, SD, area) {
   
   if (missing(effect.distance)) stop("No effect.distance argument in individualContribution")
   
@@ -33,7 +35,7 @@ individualContribution <- function(walk, ...object, .sap.poly, effect.distance,
     }
   })
   
-  message(paste("Started calculating individual contribution at:", Sys.time(), sep = " "))
+  message(paste("Preparing rasters to calculate individual contribution, starting at:", Sys.time(), sep = " "))
   
   # Create mask that will be used to subset a binned raster. sampling.area.poly
   # is added to the empty.object raster and all values inside the polygon will
@@ -65,7 +67,7 @@ individualContribution <- function(walk, ...object, .sap.poly, effect.distance,
       # plot(sap, add = TRUE)
       
       cont.in.sap <- raster::mask(x = mat, mask = raster.mask)
-      out <- cellStats(cont.in.sap, "sum")/cont.sum.ind
+      out <- sum(cont.in.sap[], na.rm = TRUE)/cont.sum.ind
       
       out
       
@@ -78,34 +80,44 @@ individualContribution <- function(walk, ...object, .sap.poly, effect.distance,
     return(out)
   }
   
-  browser()
-  
-  # For weights.yes and weights.no, calculate how much each walker contributes
-  # to the sampling area if different accumulation curves (lower and upper CI,
-  # mean) is used.
-  list.of.meco <- lapply(ring.weights, 
-                         FUN = function(w, .rst = ...object, 
-                                        .effect.distance = effect.distance, 
-                                        .empty.object = empty.object,
-                                        .raster.mask = raster.mask) {
-                           
-                           # iterate.over <- c("lower", "mean", "upper")
-                           iterate.over <- c("mean")
-                           
-                           mecos.lmu <- sapply(X = iterate.over, FUN = function(x, .medoids = medoids,
-                                                                                .object = .rst, ..effect.distance = .effect.distance,
-                                                                                ..empty.object = .empty.object, ..raster.mask = .raster.mask,
-                                                                                bin.data = w) {
-                             custom.curve <- bin.data[, x]
-                             meco <- lapply(X = .medoids, FUN = calcDistance, object = .object,
-                                            effect.distance = ..effect.distance, empty.object = ..empty.object, 
-                                            raster.mask = ..raster.mask, mask = TRUE, contr = TRUE)
-                             meco
-                           })
-                           
-                           mecos.lmu
-                         })
-  
-  message("i'm here #4")
-  list.of.meco # meco = MEdoid COntribution
+  if (sim.dist == "empirical") {
+    # Fit a model to the distribution of paired distances.
+    xd <- bins$bins$weight.yes
+    mdl <- nls(mean ~ hazardFunction(x = bins, sigma, b, mx), data = xd, 
+               start = list(sigma = mean(xd$bins),               # variance
+                            b = max(xd$bins)/(max(xd$bins)/2),   # scale parameter
+                            mx = xd$mean[1]))                    # height, since function is not scaled   
+    prs <- mdl$m$getPars() # get estimated parameters, could also use prs <- summary(mdl)
+    
+    pdf("fit_check.pdf")
+    plot(mean ~ bins, data = xd, main = sprintf("sigma: %f, xm = %f, b = %f", prs["sigma"], prs["mx"], prs["b"]))
+    curve(hazardFunction(x, sigma = prs["sigma"], b = prs["b"], mx = prs["mx"]), from = 0, to = 200, n = 200, add = TRUE)
+    dev.off()
+    
+    # Calculate distance for every cell.
+    ic <- sapply(medoids, FUN = function(xy, object, sigma, b, mx) {
+      dist.mat <- distanceFromPoints(object = object, xy = xy)
+      
+      # And based on the model above and distance from walker's medoid, find value of each cell.
+      dist.mat[] <- hazardFunction(x = dist.mat[], sigma = sigma, b = b, mx = mx)
+      
+      # plot(dist.mat, asp = 1, xlim = c(-500, 500), ylim = c(-500, 500))
+      # plot(.sap.poly, add = TRUE)
+      
+      # Scale values between 0 and 1 (trust me on this one).
+      sx <- dist.mat[]
+      dist.mat[] <- (sx - min(sx))/(max(sx) - min(sx))
+      
+      # and calculate proportion of walker's "home range" inside the sampling areas.
+      prop.all <- sum(dist.mat[], na.rm = TRUE)
+      prop.in.sap <- raster::mask(x = dist.mat, mask = raster.mask)
+      
+      sum(prop.in.sap[], na.rm = TRUE)/prop.all
+    }, object = ...object, sigma = prs["sigma"], b = prs["b"], mx = prs["mx"])
+    
+    out <- vector("list", 1)
+    out[[1]] <- as.list(ic)
+    names(out) <- "weight.yes"
+    return(out)
+  }
 }
