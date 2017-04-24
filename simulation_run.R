e64ffc3970838f93c718cceb03768894541d39ee
Roman Luštrik <romunov@gmail.com>
2017-04-24 10:51:17 +0200

    in case of en error, skip simulations already located in /data

--- simulation_run.R
+++ simulation_run.R
@@ -68,7 +68,13 @@ xy$num.boots <- 5000
 
 cl <- makeCluster(20)
 registerDoParallel(cl)
-foreach(i = 1:nrow(xy)) %dopar% {
+
+# exclude already processed runs
+
+fls <- list.files("./data", pattern = ".inp")
+nms <- as.numeric(gsub("^(.*_)(\\d+).inp$", "\\2", fls))
+
+foreach(i = (1:nrow(xy))[-nms]) %dopar% {
   library(raster)
   library(rgeos)
   library(cluster)
@@ -117,7 +123,7 @@ foreach(i = 1:nrow(xy)) %dopar% {
   warning = function(w) w)
   
   if (any(class(out) %in% c("error", "warning"))) {
-    write.table(x = rdt, file = paste(rdt$work.dir, "/failed_attempts.txt", sep = ""), row.names = FALSE, col.names = FALSE,
+    write.table(x = out, file = paste(rdt$work.dir, "/failed_attempts.txt", sep = ""), row.names = FALSE, col.names = FALSE,
                 append = TRUE, sep = ";", fileEncoding = "UTF-8")
     cat(out$message, file = "./data/failed.errors.txt", append = TRUE)
   }
