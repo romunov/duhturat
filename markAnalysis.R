f3f4b42793d8053ccf2a7173a26b4ba88f9b52e9
Roman Luštrik <romunov@gmail.com>
2017-04-28 16:30:46 +0200

    now also outputs simulation parameters

--- markAnalysis.R
+++ markAnalysis.R
@@ -5,18 +5,15 @@
 #' 
 #' @param fn Character. A (absolute) path to an .inp file. If relative, it
 #'  looks for it in the working directory.
-#' @param wd Character. Working directory where model files (\code{wd.model} 
-#'  will be deposited and the source of \code{.inp} files (\code{wd.inp}).
+#' @param wd.model Character. Working directory where model files (\code{wd.model} 
+#'  will be deposited and the source of \code{.inp} files.
 #'  
-# TODO: dodati še exporte za RMark funkcije
 
-markAnalysis <- function(fn, wd.model, wd.inp, ...) {
+markAnalysis <- function(fn, wd.model) {
 	
-  # browser()
-  
 	# READ IN RAW FILE
 	oldwd <- getwd()
-	setwd(wd.inp)
+	# setwd(wd.inp)
 	data.in <- readMark(as.character(fn))
 	
 	# DO THE ANALYSIS
@@ -32,6 +29,8 @@ markAnalysis <- function(fn, wd.model, wd.inp, ...) {
 	cml <- create.model.list("Huggins")
 	tr.result <- mark.wrapper(cml, data = tr.process, ddl = tr.ddl, adjust = FALSE,
 			invisible = TRUE)
+	
+	setwd(oldwd)
 
 	# EXTRACT DATA FROM THE ANALYSIS
 	cp.der <- data.frame(tr.result$p.cequal.dot$results$derived,
@@ -52,16 +51,15 @@ markAnalysis <- function(fn, wd.model, wd.inp, ...) {
 
 	# Calculate differences in models' AIC. If value is negative, it means that the 
 	# presumably "better" model (p(~sp)c()) is inferior and a warning is issued.
-	dAIC <- abs(tr.result$p.cequal.dot$results$AICc - tr.result$p.cequal.dot.sp$results$AICc)
-	if (dAIC <= 0) warning("AIC of p(.)=c(.)*sp is equal or worse than p(.) = c(.) ")
+	dAIC <- tr.result$p.cequal.dot$results$AICc - tr.result$p.cequal.dot.sp$results$AICc
 	
 	# OUTPUT THE DATA
 	out <- list()
 	out$real.fun.pars <- real
 	out$est.der.pars <- der
 	out$deltaAIC <- dAIC
+	out$simulation.pars <- extractPars(fn)
 	
-	setwd(oldwd)
 	
 	return(out)
 }
