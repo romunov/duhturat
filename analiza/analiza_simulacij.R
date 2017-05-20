fdb6ec9fa6761815ca9a0ac0bf44f7ddd5c09416
Roman Luštrik <romunov@gmail.com>
2017-05-20 21:57:46 +0200

    prve slike za empirično porazdelitev

--- analiza/analiza_simulacij.R
+++ analiza/analiza_simulacij.R
@@ -1,7 +1,14 @@
 # Chunk 1 load packages and scripts
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
 source("../markAnalysis.R")
 source("../readRunModels.R")
+source("../getQs.R")
+source("../calcNormal2D.R")
 
+# ======================== VANTAJM ===========================================================
 library(RMark)
 
 # Chunk 2 import data
@@ -21,61 +28,148 @@ anal.e <- sapply(data.e, FUN = markAnalysis,
                  wd.model = "./mark_intermediate/", simplify = FALSE)
 save(anal.e, file = "anal.e.RData")
 
+# ======================== END VANTAJM =======================================================
+
 rm(list = ls())
 load("anal.e.RData")
 load("anal.n.RData")
 
-# [list element, simulation number]
-unlist(sapply(anal.n[4, ], FUN = "[", "seed"))
-# TODO: pripni seed v data.frame od simulacij
+ae <- anal.e
+an <- anal.n
 
 # for each simulation, check list of 4 and do manipulations to
 # append input parameters with desired statistics
 
-anal.e <- apply(anal.e, MARGIN = 2, FUN = function(x) {
-  browser()
+ane <- sapply(ae, FUN = function(x) {
   # add population size estimates and the difference
-  x$simulation.pars$size.1 <- x$est.der.pars$N.Population.Size.estimate[1]
-  x$simulation.pars$size.sp <- x$est.der.pars$N.Population.Size.estimate[2]
-  x$simulation.pars$size.est.diff <- diff(x$est.der.pars$N.Population.Size.estimate)
+  size.1 <- x$est.der.pars$N.Population.Size.estimate[1]
+  size.sp <- x$est.der.pars$N.Population.Size.estimate[2]
+  size.est.diff <- diff(x$est.der.pars$N.Population.Size.estimate)
   
   # add capture probability esimates and the difference
-  x$simulation.pars$p.1 <- x$real.fun.pars$estimate[1]
-  x$simulation.pars$p.sp <- x$real.fun.pars$estimate[2]
-  x$simulation.pars$p.diff <- diff(x$real.fun.pars$estimate)
-  x$simulation.pars$p.target.1 <- x$simulation.pars$capture_prob - x$simulation.pars$p.1
-  x$simulation.pars$p.target.sp <- x$simulation.pars$capture_prob - x$simulation.pars$p.sp
+  p.1 <- x$real.fun.pars$estimate[1]
+  p.sp <- x$real.fun.pars$estimate[2]
+  p.diff <- diff(x$real.fun.pars$estimate)
+  p.target.1 <- x$simulation.pars$capture_prob - x$simulation.pars$p.1
+  p.target.sp <- x$simulation.pars$capture_prob - x$simulation.pars$p.sp
+  p <- x$simulation.pars$capture_prob
   
-  x$simulation.pars$dAIC <- x$deltaAIC
-
-  # naive sap area
-  x$simulation.pars$sap.area.naive <- pi * x$simulation.pars$sampling_area_r^2
-  # SAP enlarged for home range
-  x$simulation.pars$sap.area.hr <- pi * (x$simulation.pars$home_range + x$simulation.pars$sampling_area_r)^2
-  x
-})
-
-apply(anal.n, MARGIN = 2, FUN = function(x) {
-  browser()
-  # population size estimate of ~1 model
-  x$simulation.pars$size.1 <- x$est.der.pars$N.Population.Size.estimate[1]
-  # population size estimate of ~sp model
-  x$simulation.pars$size.sp <- x$est.der.pars$N.Population.Size.estimate[2]
-  # difference in population size estimates between the two models
-  x$simulation.pars$size.est.diff <- diff(x$est.der.pars$N.Population.Size.estimate)
-  
-  # add capture probability esimates and the difference
-  x$simulation.pars$p.1 <- x$real.fun.pars$estimate[1]
-  x$simulation.pars$p.sp <- x$real.fun.pars$estimate[2]
-  x$simulation.pars$p.diff <- diff(x$real.fun.pars$estimate)
-  x$simulation.pars$p.target.1 <- x$simulation.pars$capture_prob - x$simulation.pars$p.1
-  x$simulation.pars$p.target.sp <- x$simulation.pars$capture_prob - x$simulation.pars$p.sp
-  
-  x$simulation.pars$dAIC <- x$deltaAIC
+  dAIC <- x$deltaAIC # negative value means .sp bigger than .1
   
   # naive sap area
-  x$simulation.pars$sap.area.naive <- pi * x$simulation.pars$sampling_area_r^2
+  area.naive <- pi * x$simulation.pars$sampling_area_r^2
   # SAP enlarged for home range
-  x$simulation.pars$sap.area.hr <- pi * (x$simulation.pars$home_range + x$simulation.pars$sampling_area_r)^2
-  x
-})
+  area.hr <- pi * (x$simulation.pars$home_range + x$simulation.pars$sampling_area_r)^2
+  # sap enlarged for max walked distance
+  area.effect <- pi * (x$simulation.pars$sampling_area_r + x$simulation.pars$effect_distance)^2
+  
+  # based on quantile function, calculate distance for given quantiles
+  sigma <- unlist(x$simulation.pars$hazard_fun_sigma)
+  b <- unlist(x$simulation.pars$hazard_fun_b)
+  mx <- unlist(x$simulation.pars$hazard_fun_mx)
+  set.seed(x$simulation.pars$seed)
+  qs <- getQcustom(fun = weibullLikeDistribution, sigma = sigma, b = b, mx = mx, xrange = c(0, sigma * 3))
+  
+  area.50 <- pi * (x$simulation.pars$sampling_area_r + qs["50%"])^2
+  area.60 <- pi * (x$simulation.pars$sampling_area_r + qs["60%"])^2
+  area.70 <- pi * (x$simulation.pars$sampling_area_r + qs["70%"])^2
+  area.80 <- pi * (x$simulation.pars$sampling_area_r + qs["80%"])^2
+  area.90 <- pi * (x$simulation.pars$sampling_area_r + qs["90%"])^2
+  area.95 <- pi * (x$simulation.pars$sampling_area_r + qs["95%"])^2
+  area.99 <- pi * (x$simulation.pars$sampling_area_r + qs["99%"])^2
+  
+  # true density
+  dens.true <- pi * x$simulation.pars$area_size^2
+  dens.true <- x$simulation.pars$generated_walkers / dens.true
+  
+  # calculate density given (enlarged) sampling area
+  dens.naive.1 <- size.1 / area.naive
+  dens.hr.1 <- size.1 / area.hr
+  dens.effect.1 <- size.1 / area.effect
+  dens.50.1 <- size.1 / area.50
+  dens.60.1 <- size.1 / area.60
+  dens.70.1 <- size.1 / area.70
+  dens.80.1 <- size.1 / area.80
+  dens.90.1 <- size.1 / area.90
+  dens.95.1 <- size.1 / area.95
+  dens.99.1 <- size.1 / area.99
+  dens.naive.sp <- size.sp / area.naive
+  dens.hr.sp <- size.sp / area.hr
+  dens.effect.sp <- size.sp / area.effect
+  dens.50.sp <- size.sp / area.50
+  dens.60.sp <- size.sp / area.60
+  dens.70.sp <- size.sp / area.70
+  dens.80.sp <- size.sp / area.80
+  dens.90.sp <- size.sp / area.90
+  dens.95.sp <- size.sp / area.95
+  dens.99.sp <- size.sp / area.99
+  
+  # calculate index ((D - D^)/D) * 100
+  calIn <- function(Dh, D = dens.true) ((D - Dh)/D) * 100
+  
+  dens.naive.1 <- calIn(Dh = dens.naive.1)
+  dens.hr.1 <- calIn(dens.hr.1)
+  dens.effect.1 <- calIn(dens.effect.1)
+  dens.50.1 <- calIn(dens.50.1)
+  dens.60.1 <- calIn(dens.60.1)
+  dens.70.1 <- calIn(dens.70.1)
+  dens.80.1 <- calIn(dens.80.1)
+  dens.90.1 <- calIn(dens.90.1)
+  dens.95.1 <- calIn(dens.95.1)
+  dens.99.1 <- calIn(dens.99.1)
+  
+  dens.naive.sp <- calIn(dens.naive.sp)
+  dens.hr.sp <- calIn(dens.hr.sp)
+  dens.effect.sp <- calIn(dens.effect.sp)
+  dens.50.sp <- calIn(dens.50.sp)
+  dens.60.sp <- calIn(dens.60.sp)
+  dens.70.sp <- calIn(dens.70.sp)
+  dens.80.sp <- calIn(dens.80.sp)
+  dens.90.sp <- calIn(dens.90.sp)
+  dens.95.sp <- calIn(dens.95.sp)
+  dens.99.sp <- calIn(dens.99.sp)
+  
+  out <- data.frame(size.1, size.sp, # estimated sizes
+                    size.est.diff, # difference in estimated population sizes
+                    p.diff, # difference in estimated p
+                    p, # true p
+                    dAIC, # difference in AIC
+                    area.naive, # area of sap
+                    area.hr, # area of sap enlarged for home range
+                    area.effect, # area of sap enlarged for max pairwise distance recorded
+                    area.50, area.60, area.70, area.80, area.90, area.99, # sap enlarged for quantile
+                    fun = x$simulation.pars$sim.dist,  # empirical or normal
+                    hr = x$simulation.pars$home_range, # simulated home range size
+                    num.generated.walkers = x$simulation.pars$generated_walkers, # num. of sim. walkers
+                    num.captured.walkers = x$simulation.pars$num_of_sampled_walkers, # num. of sampled walkers
+                    sessions = x$simulation.pars$sessions, # number of sessions
+                    max.walk.dist = x$simulation.pars$effect_distance, # max walked distance
+                    # deviation from true density given expanded sampling areas
+                    dens.naive.1, dens.hr.1, dens.effect.1, dens.50.1, dens.60.1, dens.70.1, dens.80.1,
+                    dens.90.1, dens.95.1, dens.99.1,
+                    dens.naive.sp, dens.hr.sp, dens.effect.sp, dens.50.sp, dens.60.sp, dens.70.sp, dens.80.sp,
+                    dens.90.sp, dens.95.sp, dens.99.sp
+  )
+  out
+}, simplify = FALSE)
+
+ane <- do.call(rbind, ane)
+rownames(ane) <- NULL
+
+xy <- gather(ane, key = variable, value = index, starts_with("dens."))
+xy$variable <- factor(xy$variable)
+
+# insert some identifications
+xy$correction <- ".1"
+xy$correction[grepl(".sp$", xy$variable)] <- ".sp"
+xy$correction.type <- sapply(strsplit(xy$variable, "\\."), "[", 2)
+
+xy <- xy[xy$index > -1e+10, ]
+xy1 <- xy[sample(1:nrow(xy), size = 1000), ]
+
+ggplot(xy1, aes(x = hr, y = index, group = variable)) +
+  theme_bw() +
+  geom_point(alpha = 0.5) +
+  geom_smooth(aes(color = correction), method = "gam", k = 5) +
+  facet_wrap(~ correction.type)
+  # facet_grid(correction ~ correction.type)
