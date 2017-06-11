f5709469151ef93a44b5c2de6e6bce5b1b86b5a6
Roman Luštrik <romunov@gmail.com>
2017-06-11 14:57:51 +0200

    dodal sem še slike

--- analiza/analiza_simulacij.R
+++ analiza/analiza_simulacij.R
@@ -65,11 +65,12 @@ xe$correction.type <- sapply(strsplit(as.character(xe$variable), "\\."), "[", 2)
 xe$sap.hr.ratio <- with(xe, area.naive/hr)
 
 # first examine p
-xep <- gather(xe[, c("true.p", "p.target.1", "p.target.sp", "num.generated.walkers", "variable", "index", "sap.hr.ratio")],
+xep <- gather(xe[, c("true.p", "p.target.1", "p.target.sp", "num.generated.walkers", 
+                     "variable", "index", "sap.hr.ratio", "sessions")],
                  key = p.var, value = p.val, p.target.1, p.target.sp)
 xep$correction.type <- gsub("dens\\.(.*)\\.(.*)$", "\\1", x = xep$variable)
 xep <- xep[!duplicated(xep[, c("p.val", "p.var", "num.generated.walkers", "true.p", "correction.type")]), ]
-# xep.orig <- xep
+xep.orig <- xep
 xep <- xep.orig[sample(1:nrow(xep.orig), size = round(0.2 * nrow(xep.orig))), ]
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
@@ -77,7 +78,7 @@ ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE)
-ggsave("./figures/E-0 pristranskost .1 in .sp ocene ulovljivosti.png")
+ggsave("./figures/E-0a pristranskost .1 in .sp ocene ulovljivosti.png")
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
@@ -85,7 +86,7 @@ ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(num.generated.walkers ~ correction.type)
-ggsave("./figures/E-0 pristranskost p.1 in p.sp glede na st. walkerjev in correction type.png")
+ggsave("./figures/E-0b pristranskost p.1 in p.sp glede na st. walkerjev in correction type.png")
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
@@ -93,7 +94,7 @@ ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(num.generated.walkers ~ .)
-ggsave("./figures/E-0 pristranskost p glede na model in st. generiranih walkerjev.png")
+ggsave("./figures/E-0c pristranskost p glede na model in st. generiranih walkerjev.png")
 summary(glm(p.val ~ true.p * p.var * num.generated.walkers, data = xep))
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
@@ -102,7 +103,7 @@ ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(. ~ correction.type)
-ggsave("./figures/E0- pristranskost p glede na model in tip popravka.png")
+ggsave("./figures/E-0d pristranskost p glede na model in tip popravka.png")
 
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   theme_bw() +
@@ -110,8 +111,32 @@ ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(num.generated.walkers ~ correction.type)
-ggsave("./figures/E-0 pristranskot p glede na sap.hr ratio po st. gen. walkerjih in popravku.png")
+ggsave("./figures/E-0e pristranskot p glede na sap.hr ratio po st. gen. walkerjih in popravku.png")
 
+ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1") +
+  geom_jitter(alpha = 0.1) +
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_grid(num.generated.walkers ~ .)
+ggsave("./figures/E-0f pristranskost p glede na sap.hr ratio po st. gen walkerjev brez popravka.png")
+
+ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1") +
+  geom_jitter(alpha = 0.1) +
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_grid(. ~ correction.type)
+ggsave("./figures/E-0g pristranskost p glede na sap.hr ratio po popravku brez gostote walkerjev.png")
+
+ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1") +
+  geom_jitter(alpha = 0.1) +
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_grid(sessions ~ correction.type)
+ggsave("./figures/E-0h pristranskost p glede sap.hr razmerje glede na model.png")
+# analyse density
 # xe.orig <- xe
 # xe <- xe.orig
 xe <- xe.orig[sample(1:nrow(xe.orig), size = round(nrow(xe.orig)/10)), ]
@@ -132,57 +157,10 @@ ggplot(xe, aes(x = sap.hr.ratio, y = index)) +
   geom_jitter(alpha = 0.5, shape = 1) +
   geom_smooth(aes(color = model), method = "loess", se = FALSE) +
   scale_color_brewer(palette = "Set1") +
-  facet_grid(num.generated.walkers ~ correction.type, scales = "free_y")
-ggsave("./figures/E-1.gostota gled na razmerje hr_sap po correction type in st. gen.walk free_y.png")
-
-# density plot of p bias, do not include tirm model since we don't have p for it
-ggplot(droplevels(xe[!(xe$model %in% ".tirm"), ]), aes(x = as.factor(p), y = p.diff, fill = model)) +
-  theme_bw() +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "top") +
-  geom_violin() +
-  facet_grid(num.generated.walkers ~ sessions)
-ggsave("./figures/E-2.razlika v p glede na simuliran p po stevilu sessionov in st. gen. walk.png", 
-       width = 25, height = 18, units = "cm")
-
-ggplot(droplevels(xe[!(xe$model %in% ".tirm"), ]), aes(x = as.factor(p), y = p.diff, fill = model)) +
-  theme_bw() +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "top") +
-  geom_violin() +
-  facet_grid(num.generated.walkers ~ sessions, scales = "free_y")
-ggsave("./figures/E-2.razlika v p glede na simuliran p po stevilu sessionov in st. gen. walk free_y.png", 
-       width = 25, height = 18, units = "cm")
-
-ggplot(droplevels(xe[!(xe$model %in% ".tirm"), ]), aes(x = as.factor(p), y = p.diff, fill = model)) +
-  theme_bw() +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "top") +
-  geom_violin() +
-  facet_grid(correction.type ~ sessions)
-ggsave("./figures/E-3.razlika v p glede na simuliran p po stevilu sessionov in popravek.png",
-       width = 25, height = 18, units = "cm")
-
-ggplot(droplevels(xe[!(xe$model %in% ".tirm"), ]), aes(x = as.factor(p), y = p.diff, fill = model)) +
-  theme_bw() +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "top") +
-  geom_violin() +
-  facet_grid(correction.type ~ sessions, scales = "free_y")
-ggsave("./figures/E-3.razlika v p glede na simuliran p po stevilu sessionov in popravek free_y.png",
-       width = 25, height = 18, units = "cm")
-
-ggplot(droplevels(xe[!(xe$model %in% ".tirm"), ]), aes(x = as.factor(p), y = p.diff, fill = model)) +
-  theme_bw() +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "top") +
-  geom_violin() +
   facet_grid(num.generated.walkers ~ correction.type)
-ggsave("./figures/E-4.razlika v p glede na simuliran p po stevilu sim. walkerjev in modelu.png",
-       width = 25, height = 18, units = "cm")
-# več je vzorcev, bolj točno so ocenjeni parametri?
+ggsave("./figures")
 
-ggplot(xe, aes(x = sap.hr.ratio, y = dAIC)) +
+ggplot(droplevels(xe[xe$model %in% c(".1", ".sp"), ]), aes(x = sap.hr.ratio, y = dAIC)) +
   theme_bw() +
   theme(legend.position = "top") +
   geom_jitter(alpha = 0.5, shape = 1) +
@@ -191,21 +169,12 @@ ggplot(xe, aes(x = sap.hr.ratio, y = dAIC)) +
   facet_grid(num.generated.walkers ~ correction.type)
 ggsave("./figures/E-5.dAIC gled na razmerje hr_sap po correction type in st. gen.walk.png")
 
-ggplot(xe, aes(x = sap.hr.ratio, y = dAIC)) +
-  theme_bw() +
-  theme(legend.position = "top") +
-  geom_jitter(alpha = 0.5, shape = 1) +
-  scale_color_brewer(palette = "Set1") +
-  geom_smooth(aes(color = model), method = "loess", se = FALSE) +
-  facet_grid(num.generated.walkers ~ correction.type, scales = "free_y")
-ggsave("./figures/E-5.dAIC gled na razmerje hr_sap po correction type in st. gen.walk free y.png")
-
-ggplot(xe, aes(x = sap.hr.ratio, y = dAIC)) +
+ggplot(droplevels(xe[xe$model %in% c(".1", ".sp"), ]), aes(x = sap.hr.ratio, y = dAIC)) +
   theme_bw() +
   theme(legend.position = "top") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.5, shape = 1) +
-  geom_smooth(aes(color = better.model), method = "loess", se = FALSE) +
+  geom_smooth(aes(color = better.model), method = "lm") +
   facet_grid(num.generated.walkers ~ correction.type)
 ggsave("./figures/E-6.dAIC glede na razmerje hr_sap po correction type in st.gen.walk in boljsi model.png")
 
