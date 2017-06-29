f136248d0ed609d0f7219df35b0a02e7beedbc90
Roman Luštrik <romunov@gmail.com>
2017-06-29 16:11:18 +0200

    spremenil način računanja indeksa pristranskosti za p

--- analiza/analiza_simulacij.R
+++ analiza/analiza_simulacij.R
@@ -28,25 +28,24 @@ xep <- gather(xe[, c("true.p", "p.target.1", "p.target.sp", "num.generated.walke
 xep$correction.type <- gsub("dens\\.(.*)\\.(.*)$", "\\1", x = xep$variable)
 xep$correction.type <- factor(xep$correction.type, levels = c("naive", "hr", "50", "60", "70", "80", "90", "95", "99", "effect"))
 xep <- xep[!duplicated(xep[, c("p.val", "p.var", "num.generated.walkers", "true.p", "correction.type")]), ]
-# xep.orig <- xep
-# xep <- xep.orig[sample(1:nrow(xep.orig), size = round(0.2 * nrow(xep.orig))), ]
+xep$p.val <- xep$p.val/xep$true.p
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.05) +
-  scale_y_continuous(limits = c(0, 0.4)) +
   geom_smooth(method = "loess", se = FALSE)
 ggsave("./figures/E-0a pristranskost .1 in .sp ocene ulovljivosti.png")
 
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
+  scale_y_continuous(limits = c(-0.5, 1)) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(num.generated.walkers ~ correction.type)
 ggsave("./figures/E-0b pristranskost p.1 in p.sp glede na st. walkerjev in correction type.png",
@@ -55,9 +54,10 @@ ggsave("./figures/E-0b pristranskost p.1 in p.sp glede na st. walkerjev in corre
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
+  scale_y_continuous(limits = c(-0.5, 1)) +
   geom_smooth(method = "loess", se = FALSE) +
   facet_grid(num.generated.walkers ~ .)
 ggsave("./figures/E-0c pristranskost p glede na model in st. generiranih walkerjev.png")
@@ -66,7 +66,7 @@ summary(glm(p.val ~ true.p * p.var * num.generated.walkers, data = xep))
 ggplot(xep, aes(x = true.p, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
@@ -77,7 +77,7 @@ ggsave("./figures/E-0d pristranskost p glede na model in tip popravka.png",
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
@@ -88,7 +88,7 @@ ggsave("./figures/E-0e pristranskot p glede na sap.hr ratio po st. gen. walkerji
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
@@ -98,7 +98,7 @@ ggsave("./figures/E-0f pristranskost p glede na sap.hr ratio po st. gen walkerje
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
@@ -109,7 +109,7 @@ ggsave("./figures/E-0g pristranskost p glede na sap.hr ratio po popravku brez go
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val, color = p.var)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(method = "loess", se = FALSE) +
@@ -121,7 +121,7 @@ summary(glm(p.val ~ sap.hr.ratio * sessions * p.var, data = xep))
 ggplot(xep, aes(x = sap.hr.ratio, y = p.val)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  ylab("p underestimation") +
+  ylab("p_ests/p_true") +
   scale_color_brewer(palette = "Set1") +
   geom_jitter(alpha = 0.1) +
   geom_smooth(aes(color = sessions), method = "loess", se = FALSE) +
@@ -130,7 +130,7 @@ ggsave("./figures/E-0i pristranskost p glede na sap.hr.ratio in glede na poprave
        width = 10, height = 5, units = "in")
 
 # analyse density
-xe <- xe[xe$index > -200, ]
+xe <- xe[xe$index < 8, ]
 
 ggplot(xe, aes(x = sap.hr.ratio, y = index)) +
   theme_bw() +
@@ -138,31 +138,35 @@ ggplot(xe, aes(x = sap.hr.ratio, y = index)) +
   geom_jitter(alpha = 0.2, shape = 1) +
   geom_smooth(aes(color = model), method = "loess", se = TRUE, size = 0.5) +
   scale_color_brewer(palette = "Set1") +
-  geom_hline(yintercept = 1, size = 1, type = "dashed", alpha = 0.5) +
+  geom_hline(yintercept = 1, size = 1, alpha = 0.5) +
   facet_grid(num.generated.walkers ~ correction.type)
 ggsave("./figures/E-1.gostota gled na razmerje hr_sap po correction type in st. gen.walk.png",
        width = 10, height = 8, units = "in")
-
 
 ggplot(xe, aes(x = sap.hr.ratio, y = index)) +
   theme_bw() +
   geom_hline(yintercept = 1) +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  geom_jitter(alpha = 0.5, shape = 1) +
-  geom_smooth(aes(color = model), method = "loess", se = FALSE) +
+  geom_jitter(alpha = 0.2, shape = 1) +
+  geom_smooth(aes(color = model), method = "loess", se = TRUE, size = 0.5) +
   scale_color_brewer(palette = "Set1") +
-  geom_hline(aes(yintercept = 0)) +
+  geom_hline(yintercept = 1, size = 1, alpha = 0.5) +
   facet_grid(num.generated.walkers ~ correction.type, scales = "free_y")
-ggsave("./figures/E-1b.gostota gled na razmerje hr_sap po correction type in st. gen.walk.png")
+ggsave("./figures/E-1b.gostota gled na razmerje hr_sap po correction type in st. gen.walk.png",
+       width = 10, height = 8, units = "in")
 
 ggplot(xe, aes(x = sap.hr.ratio, y = index)) +
   theme_bw() +
   theme(legend.position = "top", axis.text.x = element_text(angle = 90, vjust = 0.5)) +
   geom_jitter(alpha = 0.5, shape = 1) +
-  geom_smooth(aes(color = model), method = "loess", se = FALSE) +
+  scale_y_continuous(limits = c(0, 2)) +
+  scale_x_continuous(limits = c(0, 5000)) +
+  geom_smooth(aes(color = model), method = "loess", se = TRUE, size = 0.5) +
   scale_color_brewer(palette = "Set1") +
+  geom_hline(yintercept = 1, size = 1, alpha = 0.5) +
   facet_grid(num.generated.walkers ~ correction.type)
-ggsave("./figures")
+ggsave("./figures/E-2.gostota glede na razmerje, corr.type in st. gen walkerjev pozoomano.png",
+       width = 10, height = 8, units = "in")
 
 # AICc
 ggplot(droplevels(xe[xe$model %in% c(".1", ".sp"), ]), aes(x = sap.hr.ratio, y = dAIC)) +
