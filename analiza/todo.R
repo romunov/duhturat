4bf957af8f920e7b1f4c0f5e7ff1edc3f0680d3d
Roman Luštrik <romunov@gmail.com>
2017-07-01 16:33:27 +0200

    things to check

---
+++ analiza/todo.R
@@ -0,0 +1,6 @@
+# preveri zakaj nekaterih razmerij ni naredil v takem številu kot druge
+
+# na primer, tole so največja razmerja (najmanji SD)
+xe[xe$sap.hr.ratio > 300, c("num.generated.walkers", "hr", "sessions", "sap.hr.ratio")]
+
+# v seznam simulacij poglej, če je kak vzorec, da kaj ni šlo skozi
